6f23b000c74d0e39f1461b126d155daa7660d90f
KeituXO <144705035+KeituXO@users.noreply.github.com>
2024-02-05 22:43:40 +0200

    Updated minor errors

--- src/app.js
+++ src/app.js
@@ -4,23 +4,22 @@ function refreshWeatherData(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let windElementElement = document.querySelector("#wind-speed");
+  let windElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new date(response.data.time * 1000);
+  let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-  cityElement.innerHTML = response.city.data;
-
+  cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
-  temperatureElement = Math.round(temperature);
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" >`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
 }
 function formatDate(date) {
-  let hours = getHours();
-  let minutes = getMinutes();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
   let days = [
     "Sunday",
     "Monday",
@@ -42,18 +41,20 @@ function searchCity(city) {
   // make the api call and update the interface
   // separation of concerns, functions to do one thing and do it well
   let apiKey = "t45f96b34088a1ob056e37a2a463d49d";
-  let apiURL =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units${metric}";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiURL).then(updateWeather);
 }
 
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-Form");
+// let cityElement = document.querySelector("#city");
+// cityElement.innerHTML = searchInput.value;
+
+let searchFormElement = document.querySelector("search-form-button");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Paris");
